6b9cc77a15b90134fa90e01dc371964ed9133a27
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-05-27 02:09:00 +0200

    chore(all): project re-structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,11 @@ fx_version 'adamant'
 game 'gta5' 
 description 'FiveM UI system' 
 version '1.0.0' 
+
 shared_scripts { 
-	'config.lua', 
-} 
-server_scripts { 
-	'server/server.lua' 
-} 
+	'shared/config.lua', 
+}
+
 client_scripts { 
 	'client/client.lua' 
 } 
